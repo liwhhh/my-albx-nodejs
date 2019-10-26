b4c64e66b318168bc6f9895abac0a2c81c1f5c6b
lwhong <li@whong.com>
2019-10-26 14:21:35 +0800

    点击图标替换完成

--- assets/js/category.js
+++ assets/js/category.js
@@ -1,16 +1,24 @@
-
+$(function () {
   $.ajax({
     type: "get",
     url: "/admin/category/getAllCategory",
     success(res) {
       if (res.code === 200) {
-        let html = template('tp',res.data);
+        let html = template('tp', res.data);
         $('tbody').html(html);
       }
     }
   });
 
   //1.切换显示隐藏
-$('.form-group > .Boxfont').on('click', function () {
-  $('.Boxfont > .right').toggle();//切换显示隐藏
+  $('.form-group > .Boxfont').on('click', function () {
+    $('.Boxfont > .right').toggle();//切换显示隐藏
+  });
+
+  //2.点击右边边框的图标
+  $('.right').on('click', '.fa', function () { //
+    let classname = $(this).attr('class');//找到当前右边框的class
+    $('.coentFont > .fa').attr('class', classname);//让做边框替换成右边框的类
+  })
+
 })
